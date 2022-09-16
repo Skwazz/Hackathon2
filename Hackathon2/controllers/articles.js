c49d8f0cf89e40a1d671eb80f07bb286a71b56dd
Dannyrobb <dannyrobb1997@gmail.com>
2022-09-16 20:13:21 +0300

    controller update

---
+++ Hackathon2/controllers/articles.js
@@ -0,0 +1,82 @@
+const { getAllArticles, getArticle, searchArticles, newArticle, updateArticle, deleteArticle } = require("../modules/articles.js");
+
+const _getAllArticles = (req, res) => {
+  getAllArticles()
+    .then((data) => {
+      res.json();
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+const _getArticle = (req, res) => {
+  getArticle(req.params.id)
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ msg: "not found" });
+      }
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+const _searchArticles = (req, res) => {
+  searchArticles(req.query.q)
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ msg: "not found" });
+      }
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+const _newArticle = (req, res) => {
+  newArticle(req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+const _updateArticle = (req, res) => {
+  updateArticle(req.params.id, req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+const _deleteArticle = (req, res) => {
+  deleteArticle(req.params.id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(404).json({ msg: "not found" });
+    });
+};
+
+module.exports = {
+  _getAllArticles,
+  _getArticle,
+  _searchArticles,
+  _newArticle,
+  _updateArticle,
+  _deleteArticle,
+};
