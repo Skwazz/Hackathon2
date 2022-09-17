2c951385f43a815548b0283d1c335924b8db86ba
Dannyrobb <dannyrobb1997@gmail.com>
2022-09-17 18:32:11 +0300

    fetch script update 12

--- Hackathon2/public/script.js
+++ Hackathon2/public/script.js
@@ -8,6 +8,9 @@
 // const { all } = require("../routes/articles");
 
 // login();
+
+const posts = document.getElementById("posts");
+
 const form = document.getElementById("form");
 function submitPost(event) {
   event.preventDefault();
@@ -29,12 +32,27 @@ function submitPost(event) {
 }
 async function allArticles() {
   const articles = await fetch("/getarticles");
-  const jsoned = await articles.json()
+  const jsoned = await articles.json();
   return jsoned;
 }
-allArticles().then((res) => {
-return res;
-}).then((res)=>{
-  
-})
+allArticles()
+  .then((res) => {
+    return res;
+  })
+  .then((res) => {
+    renderPosts(res);
+  });
 
+function renderPosts(arr) {
+  arr.forEach((post) => {
+    const postDiv = document.createElement("div");
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+    const date = document.createElement("p");
+    title.textContent = post.title;
+    body.textContent = post.body;
+    date.textContent = post.date;
+    posts.append(postDiv);
+    postDiv.append(title, body, date);
+  });
+}
